0a0b341f9e096eafb67e5f9bb60d74633296781c
Annika <annikahenry5@gmail.com>
2024-07-02 16:06:07 +0200

    fixed typo that resulted in an error

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let i = 0; i < 16 * 16; i++) {
         const gridItem = document.createElement('div');
         gridItem.classList.add('grid-item');
-        grid-gridContainer.appendChild(gridItem);
+        gridContainer.appendChild(gridItem);
     }
 });
+
+
